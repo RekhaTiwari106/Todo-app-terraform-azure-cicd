4ce060ce47a7081e55f41db8aef76efdd7448eee
RekhaTiwari106 <diwedirekha106@gmail.com>
2026-01-05 19:19:59 +0530

    added new features

--- modules/azurerm_storage_account/main.tf
+++ modules/azurerm_storage_account/main.tf
@@ -1,7 +1,6 @@
-resource "azurerm_storage_account" "example" {
-    for_each = var.storage_accounts
-
-  name                     = each.value.name
+resource "azurerm_storage_account" "SG" {
+  for_each = var.storage_accounts
+  name                     = each.value.sg_name
   resource_group_name      = each.value.resource_group_name
   location                 = each.value.location
   account_tier             = each.value.account_tier
@@ -10,5 +9,10 @@ resource "azurerm_storage_account" "example" {
   tags = each.value.tags
 }
 
-
+resource "azurerm_storage_container" "SG_container" {
+  for_each = var.storage_accounts
+  name                  = each.value.container_name
+  storage_account_id    = azurerm_storage_account.SG[each.key].id
+  container_access_type = each.value.container_access_type
+}
 
--- modules/azurerm_storage_account/variable.tf
+++ modules/azurerm_storage_account/variable.tf
@@ -1,13 +1,15 @@
 variable "storage_accounts" {
 type = map(object({
-  name = string
+  sg_name = string
   resource_group_name =string
   location = string
   account_tier =string
   account_replication_type =string
-
   tags =map(string)
-}))
 
+  container_name =string
+  container_access_type =string
+
+}))
 
 }
